92e3d553cf31de57953f4e9cdac3fb1afb448d24
Saurabh <saurabhchavhan07@gmail.com>
2022-03-26 22:11:10 +0530

    Printing Char

---
+++ Patterns/SquarePatternChar.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char n;
+    cin>>n;
+
+    for(char i='A'; i<=n; i++){
+        for(char j='A'; j<=n; j++){
+            cout<<j;
+
+        }
+        cout<<endl;
+    }
+
+
+
+
+    return 0;
+}
