368dcf226a9e0d6d08cbbdf07596e7600552a648
Saurabh <saurabhchavhan07@gmail.com>
2022-03-26 21:50:19 +0530

    Comment added

--- Patterns/SquarePattern.cpp
+++ Patterns/SquarePattern.cpp
@@ -5,9 +5,10 @@ int main()
 {
     int n;
     cin>>n;
-
+    // This "I" wala for loop run once then it goes into "J" wala for loop, it prints upto n, then move towards again "I". 
     for(int i=1; i<=n; i++)
     {
+        
         for(int j=1;j<=n; j++){
             cout<<"*";
         }
