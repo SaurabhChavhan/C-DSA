92b3d8e6269c9998b75f503120b6c9225e7d0b9d
Saurabh <saurabhchavhan07@gmail.com>
2022-03-31 22:17:17 +0530

    CPP Program on for Loops

---
+++ A Basics Getting Started/Sum5NumFor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int sum, temp;
+    sum=0;
+
+    for(int i=1; i<=5; i++){
+        cin>>temp;
+        sum+=temp;
+        
+
+    }
+    cout<<sum<<endl;
+}
