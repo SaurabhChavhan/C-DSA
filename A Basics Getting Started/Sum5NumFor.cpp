56c827799bf994e605b62658f1789969a5c38a16
Saurabh <saurabhchavhan07@gmail.com>
2022-03-31 22:36:50 +0530

    Sum Sub Mul Div of 5 numbers

--- A Basics Getting Started/Sum5NumFor.cpp
+++ A Basics Getting Started/Sum5NumFor.cpp
@@ -2,14 +2,20 @@
 using namespace std;
 
 int main(){
-    int sum, temp;
+    int sum, mul, sub, div, temp ;
     sum=0;
+    mul=1;
+    sub=0;
+    div=1;
 
     for(int i=1; i<=5; i++){
         cin>>temp;
         sum+=temp;
-        
-
+        mul*=temp;
+        sub-=temp;
+        div/=temp;
     }
-    cout<<sum<<endl;
+    cout<<"Sum is:"<<sum<<" "<<"Sub is:"<<sub<<" "<<"Mul is:"<<mul<<" "<<"Div is:"<<div<<endl;
+
+
 }
